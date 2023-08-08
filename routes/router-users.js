812fb571ca996c6d026389a16a706c2041be7f0e
Victor García <arc24system@gmail.com>
2023-08-08 13:03:49 -0600

    router users data

--- routes/router-users.js
+++ routes/router-users.js
@@ -10,3 +10,10 @@ router.post("/login", authenticateUser);
 router.post("/search", findByEmail);
 
 module.exports = router;
+
+
+// Importamos el enrutador de Express y los controladores para la gestión de usuarios
+// Creamos una instancia del enrutador
+// Definimos las rutas y los controladores correspondientes para la gestión de usuarios
+// Ruta GET para obtener todos los usuarios
+// Ruta POST para obtener el nombre completo de un usuario
