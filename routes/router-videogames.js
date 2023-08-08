8866e3312d6f2a2e7eb64c671d0f10dae1bff663
Victor García <arc24system@gmail.com>
2023-08-08 13:05:37 -0600

    router games data

--- routes/router-videogames.js
+++ routes/router-videogames.js
@@ -7,3 +7,10 @@ router.post("/findById", findById);
 router.post("/rate", rateGame);
 router.post("/", create);
 module.exports = router;
+
+
+// Importamos el enrutador de Express y los controladores para la gestión de videojuegos
+// Creamos una instancia del enrutador
+// Definimos las rutas y los controladores correspondientes para la gestión de videojuegos
+// Ruta GET para obtener todos los videojuegos
+// Ruta POST para buscar un videojuego por su ID
