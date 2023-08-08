08475ab8a256fd8d46f930a34cab3ea05b509108
Agust0124 <jaimemtzagustin@gmail.com>
2023-08-08 12:16:13 -0600

    Info Routers

--- routes/router-comments.js
+++ routes/router-comments.js
@@ -3,10 +3,18 @@ const { create, getAll, editComment, deleteComment, findByDate } = require("../c
 
 const router = Router();
 
+
 router.get("/", getAll);
 router.post("/", create);
 router.post("/search", findByDate);
 router.put("/",editComment);
 router.delete("/",deleteComment);
 
+
+
 module.exports = router;
+
+
+/*En un proyecto de Node.js utilizando el framework Express, 
+los routers desempeñan un papel esencial en la organización y estructuración de las rutas y las acciones dentro de una aplicación web. 
+Los routers permiten agrupar endpoints relacionados en un módulo separado, lo que mejora la modularidad y la legibilidad del código.  */
