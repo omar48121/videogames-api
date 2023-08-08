1aa1738948291659199535050fe078cec6c678e9
Victor García <arc24system@gmail.com>
2023-08-08 13:02:19 -0600

    router reseña data

--- routes/router-resena.js
+++ routes/router-resena.js
@@ -8,3 +8,9 @@ router.post("/", create);
 router.put("/",edit);
 router.delete("/",deleteresena);
 module.exports = router;
+
+
+// Importamos el enrutador de Express y los controladores para la gestión de reseñas
+// Creamos una instancia del enrutador
+// Definimos las rutas y los controladores correspondientes para la gestión de reseñas
+// Ruta GET para obtener todas las reseñas
